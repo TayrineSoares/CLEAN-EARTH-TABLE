0aca09b461fbe416018959ec5caabe24d5638ca5
TayrineSoares <tayrinecristina@hotmail.com>
2025-08-18 20:08:48 -0400

    route: handle /api (no trailing slash)

--- api/index.js
+++ api/index.js
@@ -6,4 +6,4 @@ const path = require('path');
 const app = require(path.join('..', 'backend', 'server.js'));
 
 // Wrap it for Vercel serverless
-module.exports = serverless(app);
+module.exports = serverless(app, { basePath: '/api' });
